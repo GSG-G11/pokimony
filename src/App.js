8fd746ffdfc08b35dab7e0c6705de5acd0b043f1
ManarAbed <manarabed333@gmail.com>
2022-04-19 22:46:32 +0300

    create header companent relates #3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
+import { Header } from './Companents'
 
 function App() {
   return (
-    <div className="App">
-      hello
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+      </div>
+    </Router>
+
   );
 }
 
